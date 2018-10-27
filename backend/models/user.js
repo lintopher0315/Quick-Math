910b65740d08ae7316a471e5312bc6f3952c99d8
lintopher0315 <lin978@purdue.edu>
2018-10-27 13:29:05 -0400

    user info on signup is now stored in user mongo database

---
+++ backend/models/user.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/customers', { useNewUrlParser: true} );
+
+var db = mongoose.connection;
+
+var UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        index: true
+    },
+    password: {
+        type: String,
+    }
+});
+
+var User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.createUser = function(newUser, callback) {
+    newUser.save(callback);
+}
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var router = express.Router();
 
+var User = require('../models/user');
+
 router.use(bodyParser.json());
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -13,6 +15,18 @@ router.post('/', function(req, res) {
     let username = req.body.usr;
     let password = req.body.pass;
     console.log(username + " " + password);
+
+    var newUser = new User({
+        username: username,
+        password: password
+    });
+
+    User.createUser(newUser, function(err, user) {
+        if (err) throw err;
+        console.log(user);
+    });
+
+
 });
 
 module.exports = router;
