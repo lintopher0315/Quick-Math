0d77619151abee346be3db2563f62b49f4c50f26
lintopher0315 <lin978@purdue.edu>
2018-11-30 17:03:37 -0500

    match document successfully removed from collection when match ends

--- src/components/QuizResults.js
+++ src/components/QuizResults.js
@@ -14,10 +14,58 @@ class QuizResults extends Component {
             opponentScore: this.props.location.state.opponentScore,
             time: this.props.location.state.seconds,
             order: this.props.location.state.order,
+            seconds: 0,
+            done: false,
         }
     }
 
+    tick() {
+        this.setState(prevState => ({
+            seconds: prevState.seconds + 1
+        }));
+
+        fetch('/users/userfinish', {
+            method: 'POST',
+            body: JSON.stringify({
+                usr: this.state.username,
+                opponent: this.state.opponent,
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(res => res.json())
+        .then(json => {
+            if (json.success) {
+                this.setState({ done: true });
+            }
+        })
+
+        if (this.state.done && this.state.order === "first") {
+            fetch('/users/removematch', {
+                method: 'POST',
+                body: JSON.stringify({
+                    usr: this.state.username,
+                }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(res => res.json())
+            .then(json => {
+                if (json.success) {
+                    console.log("removed match");
+                }
+            })
+        }
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
     componentDidMount() {
+        this.interval = setInterval(() => this.tick(), 1000)
         fetch('/users/exitmatch', {
             method: 'POST',
             body: JSON.stringify({
@@ -46,7 +94,7 @@ class QuizResults extends Component {
                     {"\n"}
                     {this.state.opponent}:{this.state.opponentScore}
                     {"\n"}
-                    Time: {this.state.seconds}
+                    Time: {this.state.time}
 
                 </div>
                 <Link to='/'>
