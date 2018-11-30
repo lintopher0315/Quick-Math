f06db5abc3e7d6b9c9cfba5cd41ea3a9188a30ea
lintopher0315 <lin978@purdue.edu>
2018-11-30 18:30:37 -0500

    added about page

--- src/components/Home.js
+++ src/components/Home.js
@@ -29,10 +29,54 @@ class Home extends Component {
                         </Link>
                     </div>
                 ): (
-                    "not logged in"
+                    <div>
+                        <div className="title" style={styles.title}>
+                            About
+                            <br />
+
+                        </div>
+                        <div className="text" style={styles.text}>
+                            <p>
+                                Quick Math is a speed math website where users can challenge each other online to see who can solve arithmetic problems the fastest.
+                            </p>
+                            <br />
+                            <p>
+                                You can begin by signing up and logging in.
+                            </p>
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <p style={styles.source}>
+                                github:
+                            </p>
+                             <a href="https://github.com/lintopher0315/Quick-Math">https://github.com/lintopher0315/Quick-Math</a>
+                        </div>
+                    </div>
                 )}
             </div>
         );
+    }
+}
+
+let styles = {
+    title: {
+        fontSize: 35,
+        fontFamily: "Roboto Mono",
+        paddingTop: 20,
+        paddingBottom: 20,
+    },
+    text: {
+        fontSize: 20,
+        fontFamily: "Roboto Mono",
+        background: "#fdfefe",
+        paddingTop: 30,
+    },
+    source: {
+        fontSize: 15,
+        fontFamily: "Roboto Mono",
+        background: "#fdfefe",
+        color: '#b20e89',
     }
 }
 
