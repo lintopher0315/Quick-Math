bd158791d4f0d4a6c420725750c34e9a224522db
lintopher0315 <lin978@purdue.edu>
2019-03-11 21:54:32 -0400

    deployment config

--- app.js
+++ app.js
@@ -84,10 +84,10 @@ app.use(function(err, req, res, next) {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
