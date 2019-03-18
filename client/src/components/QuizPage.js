a5b1c878789d92885857342253a360f9e8cb5870
lintopher0315 <lin978@purdue.edu>
2019-03-18 01:02:27 -0400

    changed question get to post

--- client/src/components/QuizPage.js
+++ client/src/components/QuizPage.js
@@ -210,7 +210,24 @@ class QuizPage extends Component {
             this.setState({ opponent: q });
         })
 
-        fetch('/users/question')
+        /*fetch('/users/question')
+        .then(res => res.json())
+        .then(json => {
+            var q = json.question;
+            var a = json.answer;
+            this.setState({ question: q, answer: a});
+            this.randomAssign(this.state.answer);
+        })*/
+
+        fetch('/users/question', {
+            method: 'POST',
+            body: JSON.stringify({
+                usr: this.state.username,
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
         .then(res => res.json())
         .then(json => {
             var q = json.question;
